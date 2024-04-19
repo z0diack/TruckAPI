edf6fd599ace62ef14d3e6407a09b70c1f0d643f
z0diack <jakevardy@gmail.com>
2024-04-19 17:09:52 +0100

    Add extra comment

--- TruckAPI/Data/DataContext.cs
+++ TruckAPI/Data/DataContext.cs
@@ -6,6 +6,7 @@ namespace TruckAPI.Data
     //Used to create an instance to the DB in use, MySQL
     public class DataContext : DbContext
     {
+        // To be used in program.cs to initialise a connection to our SQL server.
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Truck> Trucks { get; set; }
